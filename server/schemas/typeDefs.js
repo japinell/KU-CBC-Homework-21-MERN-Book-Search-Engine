6dd3e93cd574a3b1b447b6d94e5d26124c1a691a
JAPinell <japinell@yahoo.com>
2021-08-11 00:31:03 -0500

    Enabled ApolloServer, including authMiddleware, typeDefs, and Resolvers

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,34 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    password: String!
+    savedBooks: [Book]
+  }
+
+  type Book {
+    _id: ID!
+    authors: [String]
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+  }
+
+  type Query {
+    getSingleUser: [User]
+    login: [User]
+  }
+
+  type Mutation {
+    createUser(username: String!, email: String!, password: String!): User
+    saveBook(userId: ID!, bookId: ID!): User
+    deleteBook(userId: ID!, bookId: ID!): User
+  }
+`;
+
+module.exports = typeDefs;
