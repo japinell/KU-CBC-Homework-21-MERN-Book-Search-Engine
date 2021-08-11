29674750b8345a8de4906d32b8eda8dd2be9ec0a
JAPinell <japinell@yahoo.com>
2021-08-11 18:50:10 -0500

    Initial commit

--- server/server.js
+++ server/server.js
@@ -1,35 +1,21 @@
-const express = require("express");
-const path = require("path");
-const db = require("./config/connection");
-const routes = require("./routes");
-const { ApolloServer } = require("apollo-server-express");
-const { typeDefs, resolvers } = require("./schemas");
-const { authMiddleware } = require("./utils/auth");
+const express = require('express');
+const path = require('path');
+const db = require('./config/connection');
+const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware,
-});
-server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
 app.use(routes);
-console.log(url);
-db.once("open", () => {
-  app.listen(PORT, () => {
-    console.log(`Application listening on http://localhost:${PORT}`);
-    console.log(
-      `GraphQL now listening at http://localhost:${PORT}${server.graphqlPath}`
-    );
-  });
+
+db.once('open', () => {
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
